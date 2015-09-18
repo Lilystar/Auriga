df0432b3a2a4d47e07e1a53779474a549c3f20a2
Lilystar <magicalgirl.lilystar@gmail.com>
2015-09-18 19:59:20 +0900

    Auriga 0208 [2007/07/06] by Rayce

--- src/map/battle.h
+++ src/map/battle.h
@@ -42,7 +42,7 @@ struct block_list;
 // ÀÛ‚ÉHP‚ğ‘Œ¸
 int battle_delay_damage(unsigned int tick,struct block_list *src,struct block_list *target,int damage,int flag);
 int battle_damage(struct block_list *bl,struct block_list *target,int damage,int flag);
-int battle_heal(struct block_list *bl,struct block_list *target,int hp,int sp,int flag);;
+int battle_heal(struct block_list *bl,struct block_list *target,int hp,int sp,int flag);
 
 // UŒ‚ˆ—‚Ü‚Æ‚ß
 int battle_weapon_attack( struct block_list *bl,struct block_list *target,
