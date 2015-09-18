84acf299e8f6a8a990ffec2bd530b7658f597607
Lilystar <magicalgirl.lilystar@gmail.com>
2015-09-18 19:27:27 +0900

    Auriga 0197 [2007/06/27] by Rayce

--- src/map/clif.c
+++ src/map/clif.c
@@ -10188,7 +10188,7 @@ static void clif_parse_UseSkillToId(int fd,struct map_session_data *sd, int cmd)
 			if(DIFF_TICK(tick, hd->homskillstatictimer[skillnum-HOM_SKILLID]) < 0)
 				return;
 
-			if(skill_get_inf(skillnum)==4)	// Ž©•ª‚ª‘ÎÛ
+			if(skill_get_inf(skillnum) & 0x04)	// Ž©•ª‚ª‘ÎÛ
 				unit_skilluse_id(&hd->bl,hd->bl.id,skillnum,skilllv);
 			else
 				unit_skilluse_id(&hd->bl,target_id,skillnum,skilllv);
