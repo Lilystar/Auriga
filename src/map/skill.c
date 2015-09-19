90cabe376ff17c30e2d4449ae69c176ab5244f02
Lilystar <magicalgirl.lilystar@gmail.com>
2015-09-19 21:27:21 +0900

    Auriga 0566 [2008/11/03] by Rayce

--- src/map/skill.c
+++ src/map/skill.c
@@ -97,7 +97,7 @@ int SkillStatusChangeTable[MAX_SKILL] = {	/* status.h‚Ìenum‚ÌSC_***‚Æ‚ ‚í‚¹‚é‚±‚
 	/* 190- */
 	-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
 	/* 200- */
-	-1,SC_KEEPING,-1,-1,SC_BARRIER,SC_NPC_DEFENDER,-1,SC_HALLUCINATION,-1,-1,
+	-1,SC_KEEPING,-1,-1,SC_BARRIER,SC_NPC_DEFENDER,-1,SC_HALLUCINATION,SC_REBIRTH,-1,
 	/* 210- */
 	-1,-1,-1,-1,-1,SC_STRIPWEAPON,SC_STRIPSHIELD,SC_STRIPARMOR,SC_STRIPHELM,-1,
 	/* 220- */
@@ -5133,12 +5133,10 @@ int skill_castend_nodamage_id( struct block_list *src, struct block_list *bl,int
 		}
 		break;
 	case NPC_REBIRTH:
-		if(md) {
-			md->hp = mob_db[md->class_].max_hp * 10 * skilllv / 100;
+		if(md && !md->state.rebirth) {
+			// 1“x‚àƒŠƒo[ƒX‚ğg‚Á‚Ä‚È‚¢ê‡‚Ì‚İ”­“®
+			status_change_start(src,SC_REBIRTH,skilllv,0,0,0,skill_get_time(skillid,skilllv),0);
 		}
-		unit_stop_walking(src,1);
-		unit_stopattack(src);
-		unit_skillcastcancel(src,0);
 		break;
 	case NPC_RUN:		/* Œã‘Ş */
 		if(md) {
