290b57070f20edabd0d6e13765cdfa4db2da2a24
Lilystar <magicalgirl.lilystar@gmail.com>
2015-09-19 20:18:52 +0900

    Auriga 0490 [2008/05/06] by Rayce

--- src/common/core.c
+++ src/common/core.c
@@ -324,6 +324,7 @@ int main(int argc,char **argv)
 	pid_create(argv[0]);
 	do_init_memmgr(argv[0]); // ˆê”ÔÅ‰‚ÉÀs‚·‚é•K—v‚ª‚ ‚é
 	do_socket();
+	printf("PACKETVER: %d\n", PACKETVER);
 
 #ifdef _WIN32
 	SetConsoleCtrlHandler( core_CtrlHandlerRoutine, TRUE );
