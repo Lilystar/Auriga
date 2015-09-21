06a9123f316642903f9c14b58eeec63893ce11c5
Lilystar <magicalgirl.lilystar@gmail.com>
2015-09-21 22:14:29 +0900

    Auriga 0937 [2012/04/16] by Blaze

--- src/map/pc.c
+++ src/map/pc.c
@@ -6446,6 +6446,10 @@ int pc_damage(struct block_list *src,struct map_session_data *sd,int damage)
 		skill_sit(sd,0);
 	}
 
+	// ‰r¥ƒo[•\Ž¦’†‚¾‚Á‚½‚ç’†’f
+	if(sd->progressbar.npc_id)
+		clif_progressbar_abort(sd);
+
 	// •à‚¢‚Ä‚¢‚½‚ç‘«‚ðŽ~‚ß‚é
 	if(((sd->sc.data[SC_ENDURE].timer == -1 && sd->sc.data[SC_BERSERK].timer == -1 && !sd->special_state.infinite_endure) || map[sd->bl.m].flag.gvg) && !unit_isrunning(&sd->bl))
 		unit_stop_walking(&sd->bl,battle_config.pc_hit_stop_type);
